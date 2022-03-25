49eb8aede579fa0d5ce6eadc10f6639dfc4e07d5
agustinbarrios <agustin-em@hotmail.com.ar>
2022-03-25 12:17:58 -0300

    Updates

---
+++ precache-manifest.52d7b5c31c85faef2b9742ece57023ce.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f876ad69a6e0e38b21150ad4957ffb41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4d572a07036e8149141",
+    "url": "/static/css/main.cf85748f.chunk.css"
+  },
+  {
+    "revision": "a215e50ede3ffaf8105b",
+    "url": "/static/js/2.fc03f1ce.chunk.js"
+  },
+  {
+    "revision": "0def066106b5692cd5c135dcafc5767d",
+    "url": "/static/js/2.fc03f1ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4d572a07036e8149141",
+    "url": "/static/js/main.f2627ced.chunk.js"
+  },
+  {
+    "revision": "ea630fe81389fbceefbb",
+    "url": "/static/js/runtime-main.bf9f4589.js"
+  },
+  {
+    "revision": "a8268fe4abcc6f9cba72a1b15e1bf9cf",
+    "url": "/static/media/Frame.a8268fe4.png"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "12b9404883eb70c44c33c48ed4aad235",
+    "url": "/static/media/flyer.12b94048.png"
+  },
+  {
+    "revision": "6f8cde71c04ece23b35392fdbf055ef3",
+    "url": "/static/media/foto1.6f8cde71.png"
+  },
+  {
+    "revision": "c80908cc6e86b6b100cbe1c51ff2c4f7",
+    "url": "/static/media/foto2.c80908cc.png"
+  },
+  {
+    "revision": "e5d069f21a82fffc1bef5b168f93ce68",
+    "url": "/static/media/foto3.e5d069f2.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "6572290ec92eb23025f07ec0e78136cd",
+    "url": "/static/media/logo.6572290e.svg"
+  },
+  {
+    "revision": "6a36adb38886d3a19082499cbd1496f0",
+    "url": "/static/media/logo0.6a36adb3.png"
+  },
+  {
+    "revision": "b105bd44dec3dfdf5ffe2747aa7c41aa",
+    "url": "/static/media/propa1.b105bd44.png"
+  },
+  {
+    "revision": "fdd0046992e766c7e9d76eb5fb2d8e23",
+    "url": "/static/media/texto1.fdd00469.png"
+  },
+  {
+    "revision": "4483796425775876d5720aeea4a8adf5",
+    "url": "/static/media/texto2.44837964.png"
+  },
+  {
+    "revision": "0d719406793089a7a5da722e9f443307",
+    "url": "/static/media/texto3.0d719406.png"
+  }
+]);
